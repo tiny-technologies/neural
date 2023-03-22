7cc469a6d81b364d2bdad28cde5c64dbbe12d77f
Felix Andreas <felix.andreas@melli.com>
2023-03-22 23:59:30 +0100

    also free labels

--- src/lib.c
+++ src/lib.c
@@ -387,6 +387,7 @@ void destroy_dataset(Dataset dataset)
 
     for (int i = 0; i < dataset.size; i++)
     {
+        free(dataset.images[i].label);
         free(dataset.images[i].data);
     }
     free(dataset.images);
